cf9a3ad9ed1bd9ee482f2f327abd03e81ba2f0cb
Ivan Glebov <iglebov40@gmail.com>
2023-07-26 02:03:29 +0300

    feat: Add requirements.txt, .pre-commit-config.yaml and Bot Example in main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
+import logging
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+
+
+if __name__ == "__main__":
+    application = ApplicationBuilder().token("TOKEN").build()
+
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
